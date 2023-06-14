cde74502843344618e995d2239fe2107802fbae1
vinifred1003 <114234488+vinifred1003@users.noreply.github.com>
2023-06-14 11:08:03 -0300

    Update view.py
    desenvolvi mlr a aparencia do programa aumentei os botões e coloquei tela inteira

--- view.py
+++ view.py
@@ -1,5 +1,7 @@
 
 from tkinter import *
+
+
 def donothing():
     pass
 
@@ -15,7 +17,7 @@ class View():
     
     def janela(self):
       self.root.title("Projetinho")
-      self.root.geometry("650x650")
+      self.root.attributes('-fullscreen', True)
       self.root.configure(background="gray")
       self.root.resizable(True, True)
 
@@ -29,28 +31,31 @@ class View():
 ## auygsuygdyug
 
     def menu(self):
-      botao=Button(self.root, text="Relatório dos Alugueis", fg="white", bg="#7092BE")
+      botao=Button(self.root, text="Relatório dos Alugueis",fg="white", bg="#7092BE" ,height=6,width=40)
       botao.grid(column=0, row=0)
 
-      botao=Button(self.root, text=" Exibição dos Clientes   ", fg="white", bg="#7092BE")
+      botao=Button(self.root, text="Exibição dos Alugueis", fg="white", bg="#7092BE", highlightthickness=3 ,highlightbackground='#3F48CC' ,height=6,width=40)
       botao.grid(column=0, row=1)
-
-      botao=Button(self.root, text="  Cadastro dos Carros  ", fg="white", bg="#7092BE")
+      
+      botao=Button(self.root, text="Cadastro dos Carros" ,fg="white", bg="#7092BE", height=6,width=40)
       botao.grid(column=0, row=2)
 
-      botao=Button(self.root, text=" Cadastro dos Clientes ", fg="white", bg="#7092BE")
+      botao=Button(self.root, text="Cadastro dos Clientes", fg="white", bg="#7092BE", height=6,width=40)
       botao.grid(column=0, row=3)
 
-      botao=Button(self.root, text="       Marketing      ", fg="white", bg="#7092BE")
+      botao=Button(self.root, text="Exibição dos Alugueis", fg="white", bg="#7092BE", height=6,width=40)
       botao.grid(column=0, row=4)
-
-      botao=Button(self.root, text="     Configurações    ", fg="white", bg="#7092BE")
+      
+      botao=Button(self.root, text="Marketing", fg="white", bg="#7092BE", height=6,width=40)
       botao.grid(column=0, row=5)
-
-      botao=Button(self.root, text="         Ajuda        ", fg="white", bg="#7092BE")
+      
+      botao=Button(self.root, text="Configurações", fg="white", bg="#7092BE", height=6,width=40)
       botao.grid(column=0, row=6)
 
-      botao=Button(self.root, text="         Sair         ", fg="white", bg="#7092BE")
+      botao=Button(self.root, text="Ajuda", fg="white", bg="#7092BE", height=6,width=40)
       botao.grid(column=0, row=7)
+
+      botao=Button(self.root, text="Sair", fg="white", bg="#7092BE", height=6,width=40)
+      botao.grid(column=0, row=8)
 
 Janela = View()
