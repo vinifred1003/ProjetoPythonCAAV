a5bb416d195a4ad3b18a94e83bdf3acb7a538107
vinifred1003 <114234488+vinifred1003@users.noreply.github.com>
2023-06-24 16:45:42 -0300

    Update view.py

--- view.py
+++ view.py
@@ -4,16 +4,16 @@ from tkinter import *
 
 def donothing():
     pass
-
+root = Tk()
 class View():
     def __init__(self):
-      self.root = Tk()
-
+      
+      self.root = root
       self.janela()
       self.menu()
-      self.texto()
-    
-      self.root.mainloop()
+      self.Frames()
+      self.Filtros()
+      root.mainloop()
     
     def janela(self):
       self.root.title("Projetinho")
@@ -21,41 +21,51 @@ class View():
       self.root.configure(background="gray")
       self.root.resizable(True, True)
 
-    def texto(self):  
-      self.Frame_1 = Frame(self.root,bd=4, bg='gray', highlightthickness=3 ,highlightbackground='#3F48CC')
-      self.Frame_1.place(relx=0.2, rely=0, relwidth=0.8, relheight=0.2)
+    def Frames(self):  
+      self.FrameFiltros = Frame(self.root, bd=4, bg='gray', highlightthickness=3, highlightbackground='#3F48CC')
+      self.FrameFiltros.place(relx=0.2, rely=0, relwidth=0.8, relheight=0.2)
       
-      self.Frame_2 = Frame(self.root,bd=4, bg='white')
-      self.Frame_2.place(relx=0.2, rely=0.2, relwidth=0.8, relheight=0.8)
+      self.FrameRegistros = Frame(self.root, bd=4, bg='white')
+      self.FrameRegistros.place(relx=0.2, rely=0.2, relwidth=0.8, relheight=0.8)
       
-## auygsuygdyug
+    
+    def Filtros(self):
+      self.FrameFiltros = Frame(self.root, bd=4, bg='gray', highlightthickness=3, highlightbackground='#3F48CC')
+      self.FrameFiltros.place(relx=0.2, rely=0, relwidth=0.8, relheight=0.2)
+
+      self.lbId = Label(self.FrameFiltros, text="Código")
+      self.lbId.place(relx=0.1,rely=0.1)
 
     def menu(self):
-      botao=Button(self.root, text="Relatório dos Alugueis",fg="white", bg="#7092BE" ,height=6,width=40)
-      botao.grid(column=0, row=0)
+   
+      self.FrameBotoes = Frame(self.root, bd=4, bg='#3F48CC',height=1000,width=300)
+      self.FrameBotoes.grid(column=0, row=0)
 
-      botao=Button(self.root, text="Exibição dos Alugueis", fg="white", bg="#7092BE", highlightthickness=3 ,highlightbackground='#3F48CC' ,height=6,width=40)
-      botao.grid(column=0, row=1)
+      self.botaoRA=Button(self.FrameBotoes, text="Relatório dos Alugueis",fg="white", bg="#7092BE" ,height=5,width=40)
+      self.botaoRA.grid(column=0, row=0)
+
+      self.botaoEA=Button(self.FrameBotoes, text="Exibição dos Alugueis", fg="white", bg="#7092BE", highlightthickness=3 ,highlightbackground='#3F48CC' ,height=5,width=40)
+      self.botaoEA.grid(column=0, row=1)
       
-      botao=Button(self.root, text="Cadastro dos Carros" ,fg="white", bg="#7092BE", height=6,width=40)
-      botao.grid(column=0, row=2)
+      self.botaoCCar=Button(self.FrameBotoes, text="Cadastro dos Carros" ,fg="white", bg="#7092BE", height=5,width=40)
+      self.botaoCCar.grid(column=0, row=2)
 
-      botao=Button(self.root, text="Cadastro dos Clientes", fg="white", bg="#7092BE", height=6,width=40)
-      botao.grid(column=0, row=3)
+      self.botaoCC=Button(self.FrameBotoes, text="Cadastro dos Clientes", fg="white", bg="#7092BE", height=5,width=40)
+      self.botaoCC.grid(column=0, row=3)
 
-      botao=Button(self.root, text="Exibição dos Alugueis", fg="white", bg="#7092BE", height=6,width=40)
-      botao.grid(column=0, row=4)
+      self.botaoEC=Button(self.FrameBotoes, text="Exibição dos Carros", fg="white", bg="#7092BE", height=5,width=40)
+      self.botaoEC.grid(column=0, row=4)
       
-      botao=Button(self.root, text="Marketing", fg="white", bg="#7092BE", height=6,width=40)
-      botao.grid(column=0, row=5)
+      self.botaoM=Button(self.FrameBotoes, text="Marketing", fg="white", bg="#7092BE", height=5,width=40)
+      self.botaoM.grid(column=0, row=5)
       
-      botao=Button(self.root, text="Configurações", fg="white", bg="#7092BE", height=6,width=40)
-      botao.grid(column=0, row=6)
+      self.botaoC=Button(self.FrameBotoes, text="Configurações", fg="white", bg="#7092BE", height=5,width=40)
+      self.botaoC.grid(column=0, row=6)
 
-      botao=Button(self.root, text="Ajuda", fg="white", bg="#7092BE", height=6,width=40)
-      botao.grid(column=0, row=7)
+      self.botaoA=Button(self.FrameBotoes, text="Ajuda", fg="white", bg="#7092BE", height=5,width=40)
+      self.botaoA.grid(column=0, row=7)
 
-      botao=Button(self.root, text="Sair", fg="white", bg="#7092BE", height=6,width=40)
-      botao.grid(column=0, row=8)
+      self.botaoS=Button(self.FrameBotoes, text="Sair", fg="white", bg="#7092BE", height=4,width=40)
+      self.botaoS.grid(column=0, row=8)
 
 Janela = View()
