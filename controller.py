30c1332c7c587a36417f4974b23c9eed96b93bc4
vinifred1003 <114234488+vinifred1003@users.noreply.github.com>
2023-06-27 17:46:43 -0300

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,17 +1,23 @@
+from model import Model
 
-class CarroController:
+class Controller:
     
-  def __init__(self):
-    pass
+  def __init__(self, modelo=None, placa=None, marca=None, ano=None):
+
+    self.modelo = modelo
+    self.placa = placa
+    self.marca = marca
+    self.ano = ano
+    self.model = Model(self)
 
   def set(self, view, model):
     self.view = view
     self.model = model
 
-  def addCarro(self):
-    modelo = self.view.EntryModelo.get()
-    marca = self.view.EntryMarca.get()
-    placa = self.view.EntryPlaca.get()
-    ano = self.view.EntryAno.get()
-    
+  def salvar_dados(self):
+    self.model.create(self.modelo, self.placa, self.marca, self.ano)
 
+  def ler_dados(self):
+    self.model.read()
+    return self.model.resultado
+ 
