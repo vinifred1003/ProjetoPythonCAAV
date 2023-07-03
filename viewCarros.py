461c1022c215b6b84ed9001369a9ee473840f3c7
vinifred1003 <114234488+vinifred1003@users.noreply.github.com>
2023-07-03 08:31:55 -0300

    Update viewCarros.py

--- viewCarros.py
+++ viewCarros.py
@@ -13,10 +13,8 @@ class ViewCarros():
       self.Registros()
       self.controller = Controller(None, None, None, None)
       self.framesCarros()
-      dados = self.controller.ler_dados()
-      for item in dados:
-        self.listR.insert("", "end", values=item)
-      
+      self.atualizar()
+      self.limpa_campos()
 
     def run(self):
       self.root1.title("Carros Cadastrados")
@@ -32,6 +30,12 @@ class ViewCarros():
       self.Filtros()
       self.Registros()
 
+    def limpa_campos (self):
+      self.EntryId.delete(0 , END)
+      self.EntryModelo.delete(0 , END)
+      self.EntryPlaca.delete(0 , END)
+      self.EntryMarca.delete(0 , END)   
+   
     def Filtros(self):
       self.FrameFiltros = Frame(self.root1, bd=4, bg='gray', highlightthickness=3, highlightbackground='#3F48CC')
       self.FrameFiltros.place(relx=0.2, rely=0, relwidth=0.8, relheight=0.2)
@@ -42,23 +46,29 @@ class ViewCarros():
       self.EntryId = Entry(self.FrameFiltros, bd=2, bg="white")
       self.EntryId.place(relx=0.02,rely=0.45)
    
-      self.lbNome = Label(self.FrameFiltros, text="Nome", fg="white",bg="gray")
-      self.lbNome.place(relx=0.2,rely=0.28)
+      self.lbModelo = Label(self.FrameFiltros, text="Modelo", fg="white",bg="gray")
+      self.lbModelo.place(relx=0.2,rely=0.28)
 
-      self.EntryNome = Entry(self.FrameFiltros, bd=2, bg="white")
-      self.EntryNome.place(relx=0.2,rely=0.45)
+      self.EntryModelo = Entry(self.FrameFiltros, bd=2, bg="white")
+      self.EntryModelo.place(relx=0.2,rely=0.45)
 
+      self.lbMarca = Label(self.FrameFiltros, text="Marca", fg="white",bg="gray")
+      self.lbMarca.place(relx=0.4,rely=0.28)
+
+      self.EntryMarca = Entry(self.FrameFiltros, bd=2, bg="white")
+      self.EntryMarca.place(relx=0.4,rely=0.45)
+      
       self.lbPlaca = Label(self.FrameFiltros, text="Placa", fg="white",bg="gray")
-      self.lbPlaca.place(relx=0.4,rely=0.28)
+      self.lbPlaca.place(relx=0.6,rely=0.28)
 
       self.EntryPlaca = Entry(self.FrameFiltros, bd=2, bg="white")
-      self.EntryPlaca.place(relx=0.4,rely=0.45)
+      self.EntryPlaca.place(relx=0.6,rely=0.45)
       
-      self.lbCNH = Label(self.FrameFiltros, text="CNH", fg="white",bg="gray")
-      self.lbCNH.place(relx=0.6,rely=0.28)
-
-      self.EntryCNH = Entry(self.FrameFiltros, bd=2, bg="white")
-      self.EntryCNH.place(relx=0.6,rely=0.45)
+      self.BotaoDel =Button(self.FrameFiltros, text="Apagar", command=self.excluir_por_placa )
+      self.BotaoDel.place(relx=0.9, rely=0.45) 
+      
+      self.BotaoBuscar =Button(self.FrameFiltros, text="Buscar", command= self.limpa_campos )
+      self.BotaoBuscar.place(relx=0.8, rely=0.45) 
 
     def Quit(self):
       self.root1.destroy()  
@@ -105,8 +115,31 @@ class ViewCarros():
 
       self.botaoS=Button(self.FrameBotoes, text="Sair", fg="white", bg="#7092BE", height=4,width=40,command=self.Quit)
       self.botaoS.grid(column=0, row=8)
-
     
+    def atualizar(self):
+      self.listR.delete(*self.listR.get_children())
+      dados = self.controller.ler_dados()
+      for item in dados:
+        self.listR.insert("", "end", values=item)
+
+    def excluir_por_placa(self):
+        placa = self.EntryPlaca.get()
+        self.controller.excluir_dados(placa)
+        self.limpa_campos()
+        self.atualizar()
+
+    def DuploClique(self, event):
+      self.limpa_campos()
+      self.listR.selection()
+      
+     
+      for n in self.listR.selection():
+        col1, col2, col3, col4, col5 = self.listR.item(n,'values')
+        self.EntryId.insert(END, col1)
+        self.EntryModelo.insert(END, col2)
+        self.EntryMarca.insert(END, col3)
+        self.EntryPlaca.insert(END, col4)
+
     def Registros(self):
       self.FrameRegistros = Frame(self.root1, bd=4, bg='white')
       self.FrameRegistros.place(relx=0.2, rely=0.2, relwidth=0.8, relheight=0.8)
@@ -116,8 +149,8 @@ class ViewCarros():
       self.listR.heading("#0", text="")
       self.listR.heading("#1", text="Código")
       self.listR.heading("#2", text="Modelo")
-      self.listR.heading("#3", text="Placa")
-      self.listR.heading("#4", text="Marca")
+      self.listR.heading("#3", text="Marca")
+      self.listR.heading("#4", text="Placa")
       self.listR.heading("#5", text="Ano")
 
       self.listR.column("#0", width=1)
@@ -132,4 +165,4 @@ class ViewCarros():
       self.scrollListR = Scrollbar(self.FrameRegistros, orient_='vertical')
       self.listR.configure(yscroll=self.scrollListR.set)
       self.scrollListR.place(relx=0.98, rely=0.05, relwidth=0.015, relheight=0.9)
-
+      self.listR.bind("<Double-1>", self.DuploClique)
