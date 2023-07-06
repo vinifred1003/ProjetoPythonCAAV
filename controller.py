8cb946ef2581d76a8f015b4e286b100637c9fc91
vinifred1003 <114234488+vinifred1003@users.noreply.github.com>
2023-07-06 16:55:37 -0300

    Update controller.py
    Clientes foi criado

--- controller.py
+++ controller.py
@@ -2,24 +2,37 @@ from model import Model
 
 class Controller:
     
-  def __init__(self, modelo=None, placa=None, marca=None, ano=None):
-
+  def __init__(self, modelo=None, placa=None, marca=None, ano=None, nome = None, CNH = None,CPF = None,data_nasc = None,logradouro = None,numero = None ):
+    #var dos carros
     self.modelo = modelo
     self.placa = placa
     self.marca = marca
     self.ano = ano
+    #variaveis dos clientes
+    self.nome = nome
+    self.CNH = CNH
+    self.CPF = CPF
+    self.data_nasc = data_nasc
+    self.logradouro = logradouro
+    self.numero = numero
+    
+    
     self.model = Model(self)
+
 
   def set(self, view, model):
     self.view = view
     self.model = model
 
   def salvar_dados(self):
-    self.model.create(self.modelo, self.placa, self.marca, self.ano)
+    self.model.create(self.modelo, self.placa, self.marca, self.ano, self.nome,self.CNH,self.CPF,self.data_nasc,self.logradouro,self.numero)
 
   def ler_dados(self):
     self.model.read()
-    return self.model.resultado
+    return (self.model.resultado)
  
   def excluir_dados(self, placa):
     self.model.delete(placa)
+
+  def salvar_cliente(self):
+    self.model.create_cliente(self.nome,self.CNH,self.CPF,self.data_nasc,self.logradouro,self.numero)
